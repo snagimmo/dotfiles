e2bcc160ed0e132fd781b52caa059eb5973ce69c
snagimmo <31765952+snagimmo@users.noreply.github.com>
2021-02-23 21:00:12 +0900

    fixed win/install.sh

--- win/install.sh
+++ win/install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 ln -si ~/dotfiles/.config/nvim ~/.config/nvim
-ln -si ~/dotfiles/.gitconfig ~/.gitconfig
+ln -si ~/dotfiles/win/.gitconfig ~/.gitconfig
 ln -si ~/dotfiles/.vimrc ~/.vimrc
